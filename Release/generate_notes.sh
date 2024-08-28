6b847b9ccded66bfe20e70fa8e4c0295e8a803a2
Saagar Jha <saagar@saagarjha.com>
2024-08-28 00:43:33 -0700

    Turn off buffering for release notes script

--- Release/generate_notes.sh
+++ Release/generate_notes.sh
@@ -2,5 +2,8 @@
 
 set -x
 
+# Don't buffer output, as per setbuf(3)
+export STDBUF=U
+
 LAST_TAG="$(git tag --sort=-version:refname | head -2 | tail -n 1)"
 git log "$LAST_TAG"..HEAD --pretty=format:"[%as] %h: %s (%aN <%aE>)"
