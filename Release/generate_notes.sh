5fc46fe92df93c79dd946e1d2cf708332f247298
Saagar Jha <saagar@saagarjha.com>
2024-11-27 19:28:32 -0800

    Add some extra debugging to generate_notes.sh

--- Release/generate_notes.sh
+++ Release/generate_notes.sh
@@ -5,5 +5,8 @@ set -x
 # Don't buffer output, as per setbuf(3)
 export STDBUF=U
 
+ALL_TAGS="$(git tag --sort=-version:refname | xargs -n 1 git show-ref)"
+HEAD="$(git show-ref HEAD)"
+
 LAST_TAG="$(git tag --sort=-version:refname | head -2 | tail -n 1)"
 git log "$LAST_TAG"..HEAD --pretty=format:"[%as] %h: %s (%aN <%aE>)"
