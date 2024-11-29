5d4ea1e3c4e5b1f1f524321e4c97e55aacb8d97f
Saagar Jha <saagar@saagarjha.com>
2024-11-28 16:58:02 -0800

    Actually use the current tag in generate_notes.sh

--- Release/generate_notes.sh
+++ Release/generate_notes.sh
@@ -5,4 +5,4 @@ set -x
 # I'd like to use HEAD, but alas: https://github.com/actions/checkout/issues/969
 LAST_TAG="$(git tag --sort=-version:refname | head -2 | tail -n 1)"
 CURRENT_TAG="$(git tag --sort=-version:refname | head -1)"
-git log "$LAST_TAG"..main --pretty=format:"[%as] %h: %s (%aN <%aE>)"
+git log "$LAST_TAG".."$CURRENT_TAG" --pretty=format:"[%as] %h: %s (%aN <%aE>)"
